7175aeacfede014b521aa8950b216084df6214c0
matteo-didone <matteo.dido9@gmail.com>
2023-07-14 16:50:51 +0200

    Added comments to better understand AnimalProducts class

--- Classes/AnimalProducts.php
+++ Classes/AnimalProducts.php
@@ -2,12 +2,15 @@
 
 class AnimalProducts
 {
+    // Declaring food, toys and kennels as instace variables
+    // Using keyword "protected" so that they can only be accessed by the class itself and any subclasses
     protected string $food;
     protected string $toys;
 
     // Kennels is "Cuccia" in Italian
     protected string $kennels;
 
+    // Declaring constructor method for AnimalProducts class
     public function __construct(string $_food, string $_toys, string $_kennels)
     {
         $this->food = $_food;
