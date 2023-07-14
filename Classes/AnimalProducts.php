cdda1a9a1d03507e15f411ee995efda0b550b649
matteo-didone <matteo.dido9@gmail.com>
2023-07-14 16:49:55 +0200

    Created basic structure for AnimalProducts class

--- Classes/AnimalProducts.php
+++ Classes/AnimalProducts.php
@@ -1,9 +1,19 @@
 <?php
 
-class AnimalProducts{
+class AnimalProducts
+{
+    protected string $food;
+    protected string $toys;
 
-    
+    // Kennels is "Cuccia" in Italian
+    protected string $kennels;
 
+    public function __construct(string $_food, string $_toys, string $_kennels)
+    {
+        $this->food = $_food;
+        $this->toys = $_toys;
+        $this->kennels = $_kennels;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -7,36 +7,10 @@ In addition to food, the products will also include toys, kennels, etc.
 We print cards containing the details of the products, such as image, title, price, category icon, and the type of item being viewed (product, food, toy, kennel)
 */
 
-include __DIR__ . '/Models/movie.php';
-include __DIR__ . '/Models/objects.php';
-include __DIR__ . '/Models/db.php';
+include __DIR__ . '/Classes/AnimalProducts.php';
+include __DIR__ . '/Classes/DogProducts.php';
+include __DIR__ . '/Classes/CatProducts.php';;
 
-// Creating instances of Movie class and assigning them to variables $classicMovie and $modernMovie
-$classicMovie = new Movie(
-    'The Godfather',
-    1972,
-    ['Crime'],
-    'Francis Ford Coppola',
-    'R',
-    ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall'],
-    'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-    'Paramount Pictures',
-    'Paramount Pictures'
-);
-
-$modernMovie = new Movie(
-    'The Dark Knight',
-    2008,
-    ['Action', 'Crime', 'Drama'],
-    'Christopher Nolan',
-    'PG-13',
-    ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart', 'Michael Caine'],
-    'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-    'Warner Bros. Pictures',
-    'Warner Bros. Pictures'
-);
-
-$movies = [ $classicMovie, $modernMovie ];
 
 function printArray($arrayToBePrinted) {
     foreach ($arrayToBePrinted as $key => $value) {
