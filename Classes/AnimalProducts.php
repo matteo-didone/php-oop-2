c0d2b6b3b69566d233ab651dc4cc8d5c706c86d9
matteo-didone <matteo.dido9@gmail.com>
2023-07-17 18:51:01 +0200

    Updated AnimalProducts Class

--- Classes/AnimalProducts.php
+++ Classes/AnimalProducts.php
@@ -6,15 +6,26 @@ class AnimalProducts
     // Using protected instead of private to allow the child classes to access them
     protected string $image;
     protected string $title;
-
     protected string $price;
+    protected string $description;
+    protected string $category;
+    protected int $quantity;
+    protected bool $available;
+    protected string $article_type;
+    protected string $icon_category;
 
     // Constructor method 
-    public function __construct(string $image, string $title, string $price)
+    public function __construct(string $image, string $title, string $price, string $description, string $category, int $quantity, bool $available, string $article_type, string $icon_category)
     {
         $this->image = $image;
         $this->title = $title;
         $this->price = $price;
+        $this->description = $description;
+        $this->category = $category;
+        $this->quantity = $quantity;
+        $this->available = $available;
+        $this->article_type = $article_type;
+        $this->icon_category = $icon_category;
     }
 
     // Getters and Setters
@@ -46,6 +57,66 @@ class AnimalProducts
     public function setPrice(string $price)
     {
         $this->price = $price;
+    }
+
+    public function getArticleType()
+    {
+        return $this->article_type;
+    }
+
+    public function setArticleType(string $article_type)
+    {
+        $this->article_type = $article_type;
+    }
+
+    public function getIconCategory()
+    {
+        return $this->icon_category;
+    }
+
+    public function setIconCategory(string $icon_category)
+    {
+        $this->icon_category = $icon_category;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->$description = $description;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category)
+    {
+        $this->$category = $category;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity)
+    {
+        $this->$quantity = $quantity;
+    }
+
+    public function getAvailable()
+    {
+        return $this->available;
+    }
+
+    public function setAvailable(bool $available)
+    {
+        $this->$available = $available;
     }
 
 }
